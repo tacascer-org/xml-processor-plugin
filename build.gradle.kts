79f712c96588f4cc9281a133a2858222670ca274
tacascer <trandangtrithanh2000@gmail.com>
2024-04-11 00:05:38 -0400

    ci: configure publishing for plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,10 +57,9 @@ gradlePlugin {
 }
 
 signing {
-    val signingKeyId: String? by project
     val signingKey: String? by project
     val signingPassword: String? by project
-    useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
+    useInMemoryPgpKeys(signingKey, signingPassword)
 }
 
 tasks.sonar {
