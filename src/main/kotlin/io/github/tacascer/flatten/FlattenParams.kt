55146ba9a4e7d3905af72bff416053b022500880
Tim Tran <49887921+tacascer@users.noreply.github.com>
2024-04-16 23:32:04 -0400

    feat: can strip namespaces (#18)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **New Features**
    - Introduced a new feature to strip namespaces from XML schemas during
    flattening.
    - Added a new configuration option to enable or disable the stripping of
    namespaces in XML processing tasks.
    
    - **Documentation**
    - Updated the plugin description and adjusted the style of the Gradle
    plugin portal version badge.
    
    - **Tests**
    - Added new test configurations and a specific test to verify the new
    namespace stripping functionality.
    
    - **Chores**
    - Updated XML processor version to enhance performance and
    compatibility.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- src/main/kotlin/io/github/tacascer/flatten/FlattenParams.kt
+++ src/main/kotlin/io/github/tacascer/flatten/FlattenParams.kt
@@ -1,8 +1,10 @@
 package io.github.tacascer.flatten
 
 import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.provider.Property
 
 interface FlattenParams {
     val inputFile: RegularFileProperty
     val outputFile: RegularFileProperty
+    val stripNamespaces: Property<Boolean>
 }
--- src/main/kotlin/io/github/tacascer/flatten/XMLFlattenTask.kt
+++ src/main/kotlin/io/github/tacascer/flatten/XMLFlattenTask.kt
@@ -3,6 +3,8 @@ package io.github.tacascer.flatten
 import io.github.tacascer.XmlIncludeFlattener
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
@@ -14,10 +16,13 @@ abstract class XMLFlattenTask : DefaultTask() {
     @get:OutputFile
     abstract val outputFile: RegularFileProperty
 
+    @get:Input
+    abstract val stripNamespaces: Property<Boolean>
+
     @TaskAction
     fun flatten() {
         val file = outputFile.get().asFile
-        val flattener = XmlIncludeFlattener(inputFile.get().asFile.toPath())
+        val flattener = XmlIncludeFlattener(inputFile.get().asFile.toPath(), stripNamespace = stripNamespaces.get())
         file.parentFile.mkdirs()
         flattener.process(file.toPath())
     }
