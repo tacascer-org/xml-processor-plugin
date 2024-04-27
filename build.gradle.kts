ff01cc1091e8144fa4c70a746104104eb1091a28
Tim Tran <49887921+tacascer@users.noreply.github.com>
2024-04-27 12:54:07 -0400

    feat: Upgrade xml (#28)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **New Features**
    - Updated XML processing capabilities, enhancing performance and
    reliability.
    - **Bug Fixes**
    - Removed outdated configurations related to XML namespace handling to
    prevent processing errors.
    - **Refactor**
    - Simplified XML flattening logic for improved maintainability and
    efficiency.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ val kotestVersion = "5.8.1"
 val jetbrainsAnnotationVersion = "24.1.0"
 val slf4jSimpleVersion = "2.0.13"
 val kotlinLoggingVersion = "6.0.9"
-val xmlProcessorVersion = "0.4.0"
+val xmlProcessorVersion = "0.6.0"
 
 dependencies {
     compileOnly("org.jetbrains:annotations:$jetbrainsAnnotationVersion")
--- src/test/kotlin/io/github/tacascer/flatten/XmlFlattenTaskTest.kt
+++ src/test/kotlin/io/github/tacascer/flatten/XmlFlattenTaskTest.kt
@@ -43,49 +43,6 @@ class XmlFlattenTaskTest : FunSpec({
             it.apply {
                 inputFile.set(includingFile.toFile())
                 outputFile.set(testOutputFile.toFile())
-                stripNamespaces.set(false)
-            }
-        }
-
-        xmlFlatten.get().flatten()
-
-        testOutputFile.readText().trimIndent() shouldBe expectedText
-    }
-
-    test("XmlFlattenTask should strip namespace from final file if stripNamespaces is true") {
-        val testDir = tempdir()
-        val includingFile = testDir.toPath().resolve("sample.xsd").createFile()
-        val includedFile = testDir.toPath().resolve("sample_1.xsd").createFile()
-        val testOutputFile = testDir.toPath().resolve("output.xsd").createFile()
-        @Language("XML") val includingFileText = """
-            <?xml version="1.0" encoding="UTF-8"?>
-            <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema" targetNamespace="http://www.sample.com">
-                <xs:include schemaLocation="${testDir.toPath().resolve("sample_1.xsd").toUri()}"/>
-                <xs:element name="sample" type="xs:string"/>
-            </xs:schema>
-            """.trimIndent()
-        includingFile.writeText(includingFileText)
-        @Language("XML") val includedFileText = """
-            <?xml version="1.0" encoding="UTF-8"?>
-            <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema">
-                <xs:element name="sampleOne" type="xs:string"/>
-            </xs:schema>
-            """.trimIndent()
-        includedFile.writeText(includedFileText)
-
-        @Language("XML") val expectedText = """
-        <?xml version="1.0" encoding="UTF-8"?>
-        <schema targetNamespace="http://www.sample.com">
-            <element name="sample" type="string" />
-            <element name="sampleOne" type="string" />
-        </schema>
-        """.trimIndent()
-        val project = ProjectBuilder.builder().build()
-        val xmlFlatten = project.tasks.register("xmlFlatten", XMLFlattenTask::class.java) {
-            it.apply {
-                inputFile.set(includingFile.toFile())
-                outputFile.set(testOutputFile.toFile())
-                stripNamespaces.set(true)
             }
         }
 
