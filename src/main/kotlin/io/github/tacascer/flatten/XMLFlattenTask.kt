2f8d13a75fdfec99ff25f718570605d39065138f
tacascer <trandangtrithanh2000@gmail.com>
2024-04-27 12:29:31 -0400

    build: update XMLProcessor version

--- src/main/kotlin/io/github/tacascer/flatten/XMLFlattenTask.kt
+++ src/main/kotlin/io/github/tacascer/flatten/XMLFlattenTask.kt
@@ -1,10 +1,7 @@
 package io.github.tacascer.flatten
 
-import io.github.tacascer.XmlIncludeFlattener
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.RegularFileProperty
-import org.gradle.api.provider.Property
-import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
@@ -16,14 +13,11 @@ abstract class XMLFlattenTask : DefaultTask() {
     @get:OutputFile
     abstract val outputFile: RegularFileProperty
 
-    @get:Input
-    abstract val stripNamespaces: Property<Boolean>
-
     @TaskAction
     fun flatten() {
         val file = outputFile.get().asFile
-        val flattener = XmlIncludeFlattener(inputFile.get().asFile.toPath(), stripNamespace = stripNamespaces.get())
+        val flattener = IncludeFlattener()
         file.parentFile.mkdirs()
-        flattener.process(file.toPath())
+        flattener.process(inputFile.get().asFile.toPath(), file.toPath())
     }
 }
